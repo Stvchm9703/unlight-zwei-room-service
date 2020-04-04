72270194a1b7fbc9ddbd3d790ef192527a1c3653
steven <stv.chm@gmail.com>
2020-04-05 01:46:09 +0800

    redefine the broadcast

--- proto/service.pb.go
+++ proto/service.pb.go
@@ -29,33 +29,33 @@ const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 func init() { proto.RegisterFile("service.proto", fileDescriptor_a0b84a42fa06f626) }
 
 var fileDescriptor_a0b84a42fa06f626 = []byte{
-	// 267 bytes of a gzipped FileDescriptorProto
+	// 266 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2d, 0x4e, 0x2d, 0x2a,
 	0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x08, 0xf5, 0x89, 0x0a, 0x00,
 	0xb1, 0xa4, 0x78, 0x73, 0x53, 0x8b, 0x8b, 0x13, 0xd3, 0xa1, 0x12, 0x52, 0x3c, 0xc9, 0xf9, 0xb9,
-	0xb9, 0xf9, 0x79, 0x10, 0x9e, 0xd1, 0x1f, 0x66, 0x2e, 0xee, 0xa0, 0xfc, 0xfc, 0xdc, 0x60, 0x88,
-	0x66, 0x21, 0x73, 0x2e, 0x2e, 0xe7, 0xa2, 0xd4, 0xc4, 0x92, 0x54, 0x90, 0xa0, 0x90, 0xb8, 0x1e,
-	0xcc, 0x14, 0x3d, 0x10, 0x1f, 0x2a, 0x93, 0x5a, 0x28, 0xc5, 0x87, 0x2a, 0xa1, 0xc4, 0x20, 0x64,
-	0xc5, 0xc5, 0xed, 0x9e, 0x5a, 0x02, 0xe2, 0xf8, 0x64, 0x16, 0x97, 0x90, 0xa0, 0xd3, 0x80, 0x51,
-	0xc8, 0x08, 0xae, 0xd7, 0x33, 0x2f, 0x2d, 0x5f, 0x48, 0x10, 0x55, 0x09, 0x76, 0xfb, 0xcc, 0xb9,
-	0xb8, 0x42, 0x0b, 0x52, 0xc8, 0x70, 0xa8, 0x35, 0x17, 0x3f, 0xc8, 0xb3, 0xa9, 0x45, 0x4e, 0x45,
-	0xf9, 0x89, 0x29, 0xc9, 0x89, 0xc5, 0x25, 0xd8, 0x2c, 0x44, 0x13, 0xf2, 0x2d, 0x4e, 0x07, 0xbb,
-	0xd4, 0x98, 0x8b, 0x3b, 0x38, 0x35, 0x2f, 0xc5, 0x17, 0x12, 0xa2, 0x42, 0x98, 0xaa, 0xa4, 0xf8,
-	0x11, 0x42, 0xae, 0xb9, 0x05, 0x25, 0x95, 0x4a, 0x0c, 0x42, 0x06, 0x5c, 0x1c, 0x81, 0xa5, 0x99,
-	0x60, 0xff, 0x61, 0xb3, 0x0a, 0x8b, 0x0e, 0x33, 0x2e, 0xce, 0xc0, 0xd2, 0xcc, 0xe4, 0xec, 0x80,
-	0xc4, 0xcc, 0x22, 0x52, 0xfc, 0xa6, 0xcf, 0xc5, 0xe1, 0x95, 0x9f, 0x99, 0x87, 0xcb, 0x26, 0x0c,
-	0x0d, 0x4e, 0xd2, 0x51, 0xac, 0xe0, 0x74, 0xb0, 0x8a, 0x49, 0x30, 0xd4, 0x27, 0xca, 0xb1, 0xb8,
-	0x38, 0xb5, 0x44, 0x0f, 0xac, 0xc0, 0x37, 0x3f, 0x25, 0x89, 0x0d, 0x2c, 0x65, 0x0c, 0x08, 0x00,
-	0x00, 0xff, 0xff, 0x6b, 0x39, 0xff, 0x72, 0x5a, 0x02, 0x00, 0x00,
+	0xb9, 0xf9, 0x79, 0x10, 0x9e, 0x51, 0x13, 0x0b, 0x17, 0x77, 0x50, 0x7e, 0x7e, 0x6e, 0x30, 0x44,
+	0xb3, 0x90, 0x39, 0x17, 0x97, 0x73, 0x51, 0x6a, 0x62, 0x49, 0x2a, 0x48, 0x50, 0x48, 0x5c, 0x0f,
+	0x66, 0x8a, 0x1e, 0x88, 0x0f, 0x95, 0x49, 0x2d, 0x94, 0xe2, 0x43, 0x95, 0x50, 0x62, 0x10, 0xb2,
+	0xe2, 0xe2, 0x76, 0x4f, 0x2d, 0x01, 0x71, 0x7c, 0x32, 0x8b, 0x4b, 0x48, 0xd0, 0x69, 0xc0, 0x28,
+	0x64, 0x04, 0xd7, 0xeb, 0x99, 0x97, 0x96, 0x2f, 0x24, 0x88, 0xaa, 0x04, 0xbb, 0x7d, 0xe6, 0x5c,
+	0x5c, 0xa1, 0x05, 0x29, 0x64, 0x38, 0xd4, 0x91, 0x8b, 0xd7, 0xa9, 0x28, 0x3f, 0x31, 0x25, 0x39,
+	0xb1, 0xb8, 0x04, 0x97, 0x75, 0xd2, 0xa8, 0x42, 0x28, 0xea, 0x95, 0x18, 0x84, 0x8c, 0xb9, 0xb8,
+	0x83, 0x53, 0xf3, 0x52, 0x7c, 0x21, 0xe1, 0x8a, 0x6e, 0x80, 0x6f, 0x71, 0xba, 0x14, 0x3f, 0x42,
+	0xc8, 0x35, 0xb7, 0xa0, 0xa4, 0x52, 0x89, 0x41, 0xc8, 0x80, 0x8b, 0x23, 0xb0, 0x34, 0x13, 0xec,
+	0x4b, 0x6c, 0x56, 0x62, 0xd1, 0x61, 0xc6, 0xc5, 0x19, 0x58, 0x9a, 0x99, 0x9c, 0x1d, 0x90, 0x98,
+	0x59, 0x44, 0x8a, 0x0f, 0xf5, 0xb9, 0x38, 0xbc, 0xf2, 0x33, 0xf3, 0x70, 0xd9, 0x84, 0xa1, 0xc1,
+	0x49, 0x3a, 0x8a, 0x15, 0x9c, 0x1a, 0x56, 0x31, 0x09, 0x86, 0xfa, 0x44, 0x39, 0x16, 0x17, 0xa7,
+	0x96, 0xe8, 0x81, 0x15, 0xf8, 0xe6, 0xa7, 0x24, 0xb1, 0x81, 0xa5, 0x8c, 0x01, 0x01, 0x00, 0x00,
+	0xff, 0xff, 0x00, 0x25, 0xea, 0xa3, 0x60, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // RoomServiceClient is the client API for RoomService service.
 //
@@ -65,7 +65,7 @@ type RoomServiceClient interface {
 	GetRoomList(ctx context.Context, in *RoomCreateReq, opts ...grpc.CallOption) (RoomService_GetRoomListClient, error)
 	GetRoomInfo(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (*Room, error)
 	UpdateRoom(ctx context.Context, in *RoomCreateReq, opts ...grpc.CallOption) (*Room, error)
-	ServerBroadcast(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (RoomService_ServerBroadcastClient, error)
+	BroadcastInfo(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (*RoomBroadcastInfo, error)
 	SendMessage(ctx context.Context, in *RoomMsg, opts ...grpc.CallOption) (*Empty, error)
 	QuitRoom(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (*Empty, error)
 	QuickPair(ctx context.Context, in *RoomCreateReq, opts ...grpc.CallOption) (*Room, error)
@@ -73,10 +73,10 @@ type RoomServiceClient interface {
 }
 
 type roomServiceClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewRoomServiceClient(cc *grpc.ClientConn) RoomServiceClient {
+func NewRoomServiceClient(cc grpc.ClientConnInterface) RoomServiceClient {
 	return &roomServiceClient{cc}
 }
 
@@ -139,36 +139,13 @@ func (c *roomServiceClient) UpdateRoom(ctx context.Context, in *RoomCreateReq, o
 	return out, nil
 }
 
-func (c *roomServiceClient) ServerBroadcast(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (RoomService_ServerBroadcastClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_RoomService_serviceDesc.Streams[1], "/ULZProto.RoomService/ServerBroadcast", opts...)
+func (c *roomServiceClient) BroadcastInfo(ctx context.Context, in *RoomReq, opts ...grpc.CallOption) (*RoomBroadcastInfo, error) {
+	out := new(RoomBroadcastInfo)
+	err := c.cc.Invoke(ctx, "/ULZProto.RoomService/BroadcastInfo", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &roomServiceServerBroadcastClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type RoomService_ServerBroadcastClient interface {
-	Recv() (*RoomMsg, error)
-	grpc.ClientStream
-}
-
-type roomServiceServerBroadcastClient struct {
-	grpc.ClientStream
-}
-
-func (x *roomServiceServerBroadcastClient) Recv() (*RoomMsg, error) {
-	m := new(RoomMsg)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
+	return out, nil
 }
 
 func (c *roomServiceClient) SendMessage(ctx context.Context, in *RoomMsg, opts ...grpc.CallOption) (*Empty, error) {
@@ -213,7 +190,7 @@ type RoomServiceServer interface {
 	GetRoomList(*RoomCreateReq, RoomService_GetRoomListServer) error
 	GetRoomInfo(context.Context, *RoomReq) (*Room, error)
 	UpdateRoom(context.Context, *RoomCreateReq) (*Room, error)
-	ServerBroadcast(*RoomReq, RoomService_ServerBroadcastServer) error
+	BroadcastInfo(context.Context, *RoomReq) (*RoomBroadcastInfo, error)
 	SendMessage(context.Context, *RoomMsg) (*Empty, error)
 	QuitRoom(context.Context, *RoomReq) (*Empty, error)
 	QuickPair(context.Context, *RoomCreateReq) (*Room, error)
@@ -236,8 +213,8 @@ func (*UnimplementedRoomServiceServer) GetRoomInfo(ctx context.Context, req *Roo
 func (*UnimplementedRoomServiceServer) UpdateRoom(ctx context.Context, req *RoomCreateReq) (*Room, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateRoom not implemented")
 }
-func (*UnimplementedRoomServiceServer) ServerBroadcast(req *RoomReq, srv RoomService_ServerBroadcastServer) error {
-	return status.Errorf(codes.Unimplemented, "method ServerBroadcast not implemented")
+func (*UnimplementedRoomServiceServer) BroadcastInfo(ctx context.Context, req *RoomReq) (*RoomBroadcastInfo, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method BroadcastInfo not implemented")
 }
 func (*UnimplementedRoomServiceServer) SendMessage(ctx context.Context, req *RoomMsg) (*Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SendMessage not implemented")
@@ -331,25 +308,22 @@ func _RoomService_UpdateRoom_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
-func _RoomService_ServerBroadcast_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(RoomReq)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
+func _RoomService_BroadcastInfo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RoomReq)
+	if err := dec(in); err != nil {
+		return nil, err
 	}
-	return srv.(RoomServiceServer).ServerBroadcast(m, &roomServiceServerBroadcastServer{stream})
-}
-
-type RoomService_ServerBroadcastServer interface {
-	Send(*RoomMsg) error
-	grpc.ServerStream
-}
-
-type roomServiceServerBroadcastServer struct {
-	grpc.ServerStream
-}
-
-func (x *roomServiceServerBroadcastServer) Send(m *RoomMsg) error {
-	return x.ServerStream.SendMsg(m)
+	if interceptor == nil {
+		return srv.(RoomServiceServer).BroadcastInfo(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/ULZProto.RoomService/BroadcastInfo",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RoomServiceServer).BroadcastInfo(ctx, req.(*RoomReq))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _RoomService_SendMessage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -441,6 +415,10 @@ var _RoomService_serviceDesc = grpc.ServiceDesc{
 			Handler:    _RoomService_UpdateRoom_Handler,
 		},
 		{
+			MethodName: "BroadcastInfo",
+			Handler:    _RoomService_BroadcastInfo_Handler,
+		},
+		{
 			MethodName: "SendMessage",
 			Handler:    _RoomService_SendMessage_Handler,
 		},
@@ -461,11 +439,6 @@ var _RoomService_serviceDesc = grpc.ServiceDesc{
 		{
 			StreamName:    "GetRoomList",
 			Handler:       _RoomService_GetRoomList_Handler,
-			ServerStreams: true,
-		},
-		{
-			StreamName:    "ServerBroadcast",
-			Handler:       _RoomService_ServerBroadcast_Handler,
 			ServerStreams: true,
 		},
 	},
